6ff67c4619461445eacdd3b6d52634dfd32c2b84
danielsosauy <dsosa@spacedev.io>
2022-10-17 11:01:49 -0300

    fix: enable exhaustive-deps

--- template/src/design/input/TextInput/TextInput.tsx
+++ template/src/design/input/TextInput/TextInput.tsx
@@ -70,7 +70,6 @@ export const TextInput = (props: Props): JSX.Element | null => {
     }
 
     return materialTheme;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [color, materialTheme]);
 
   if (system === DesignSystem.MATERIAL_UI) {
