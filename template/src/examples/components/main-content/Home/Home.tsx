bfe6538eeca9c483e3b5bbf2b3c7a4385ee1b833
Joaquin Ferreira <jferreira@spacedev.uy>
2022-10-26 10:08:32 -0300

    fix: add default theme modes list

--- template/src/examples/components/main-content/Home/Home.tsx
+++ template/src/examples/components/main-content/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { supportedThemeModes } from 'config/mode-theme.config';
 import { Card, Select, Text, Well } from 'design';
 import { DesignContext } from 'design/DesignContext';
 import { DesignSystem } from 'design/enums/design-system.enum';
@@ -29,7 +30,7 @@ export const Home: FC = () => {
         }),
       )
       .then((users) =>
-        console.log('users:', users.map((item) => item.name).join(', ')),
+        console.log('users:', users.map((item) => item.name).join(', '), supportedThemeModes),
       )
       .catch(genericErrorHandler);
   }, []);
