46d27de72b50118711ed62d36eecb2f02492b27c
Joaquin Ferreira <jferreira@spacedev.uy>
2022-10-26 10:08:32 -0300

    fix: add default theme modes list

--- template/src/config/index.ts
+++ template/src/config/index.ts
@@ -1,5 +1,6 @@
 import * as http from './http.config';
+import * as modeThemes from './mode-theme.config';
 import * as validation from './validation.config';
 import * as walletProvider from './wallet-provider.config';
 
-export const config = { http, validation, walletProvider };
+export const config = { http, validation, walletProvider, modeThemes };
---
+++ template/src/config/mode-theme.config.ts
@@ -0,0 +1,6 @@
+const themes = {
+  DARK: 'dark',
+  LIGHT: 'light',
+};
+
+export const supportedThemeModes = [themes.DARK, themes.LIGHT];
