28438516fa79097ec933f8f7a53e38161f1a2b06
danielspaceuy <70650789+danielspaceuy@users.noreply.github.com>
2022-10-28 09:23:13 -0300

    refactor: move isDarkPreferred inside function

--- template/src/design/helpers/theme.helpers.ts
+++ template/src/design/helpers/theme.helpers.ts
@@ -9,11 +9,11 @@ export function getStoredThemeMode(): ThemeMode | null {
   return storage.local.get(StorageItem.THEME_MODE);
 }
 
-const isDarkPreferred =
-  window.matchMedia('(prefers-color-scheme: dark)').matches &&
-  config.theme.supportedThemeModes.includes(ThemeMode.DARK);
-
 export function getPreferedThemeMode(): ThemeMode {
+  const isDarkPreferred =
+    window.matchMedia('(prefers-color-scheme: dark)').matches &&
+    config.theme.supportedThemeModes.includes(ThemeMode.DARK);
+      
   return isDarkPreferred ? ThemeMode.DARK : ThemeMode.LIGHT;
 }
 
